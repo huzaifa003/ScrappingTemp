ebaa3fdb30754d258315ced36941dfef7b8d03a2
Huzaifa Imran <51008597+huzaifa003@users.noreply.github.com>
2024-04-20 14:27:59 +0000

    Added App.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -9,24 +8,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-
+import webdriver_manager
+import webdriver_manager.chrome 
 app = Flask(__name__)
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 def initialize_driver():
     # Configure ChromeOptions with desired arguments
+    
     chrome_options = webdriver.ChromeOptions()
+    user_agent_string = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    chrome_options.add_argument(f"user-agent={user_agent_string}")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     
-    # Set custom user-agent string
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    
+    # chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--enable-logging=stderr")
+    chrome_options.add_argument("--v=1")
+
+    # chrome_options.add_argument("--user-data-dir=/abc/profile")
+
     # Initialize ChromeDriver using ChromeDriverManager to handle driver installation
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
+
     return driver
+
 
 # Initialize drivers and set global variables
 driver1 = initialize_driver()
@@ -43,6 +54,7 @@ driver2 = initialize_driver()
 
 # Example scraping logic, replace with actual scraping logic
 driver1.get("https://www.investing.com/crypto/bitcoin/technical")
+print(driver1.title)
 driver2.get("https://www.tradingview.com/symbols/BTCUSD/technicals/?exchange=CRYPTO")
     
 # Global variables to store scraped data
@@ -78,18 +90,23 @@ def scrape_both(driver1, driver):
     driver.find_element(By.ID, "1m").click()
 
     # Replace with the actual URL where the tables are located
-    driver1.get("https://www.investing.com/crypto/bitcoin/technical")
-
+    # driver1.get("https://www.investing.com/crypto/bitcoin/technical")
+    # driver1.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     print(driver1.title)
     button = driver1.execute_script('return document.querySelector("[data-test=\'1m\']");')
+    # dynamic_table_body = WebDriverWait(driver1, timeout=50).until(
+    #     EC.presence_of_element_located((By.CSS_SELECTOR, '.datatable_body__tb4jX'))
+    # )
+    
 
 	# You can now interact with 'element' as a Selenium WebElement object
 	# For example, to get text from the element:from selenium.webdriver.common.action_chains import ActionChains
     #WebDriverWait(driver,50).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#__next > div.desktop\:relative.desktop\:bg-background-default > div.relative.flex > div.grid.flex-1.grid-cols-1.px-4.pt-5.font-sans-v2.text-\[\#232526\].antialiased.xl\:container.sm\:px-6.md\:grid-cols-\[1fr_72px\].md\:gap-6.md\:px-7.md\:pt-10.md2\:grid-cols-\[1fr_420px\].md2\:gap-8.md2\:px-8.xl\:mx-auto > div.min-w-0 > div:nth-child(5) > div > div.mb-14.md\:mb-16 > div > button.relative.inline-flex.items-center.justify-center.whitespace-nowrap.rounded-sm.p-1\.5.text-xs.font-bold.leading-tight.text-primary.no-underline.disabled\:text-disabled.text-xs.text-v2-gray-dark.bg-v2-gray-light-2.py-2.px-4.\!rounded-\[3px\].ml-2.first\:ml-0.\!text-v2-blue.\!bg-v2-blue-dark')))
     #target_button = WebDriverWait(driver, 50).until(
      #   EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test="1m"]')))
-    actions = ActionChains(driver1)
-    actions.move_to_element(button).click().perform()
+    if button:
+        actions = ActionChains(driver1)
+        actions.move_to_element(button).click().perform()
     
        
     simple_keys_dict1 = {
@@ -107,9 +124,9 @@ def scrape_both(driver1, driver):
 
 
     vals_to_get_1 = ['Relative Strength Index (14)', 'Commodity Channel Index (20)', 'Average Directional Index (14)', 'Stochastic RSI Fast (3, 3, 14, 14)', 'Williams Percent Range (14)', 'Ultimate Oscillator (7, 14, 28)', 'Bull Bear Power', 'Simple Moving Average (10)', 'Simple Moving Average (20)', 'Simple Moving Average (50)' ]
-    dict1 = {}
+    
     vals_to_get_2 = ['RSI(14)', 'CCI(14)', 'ADX(14)', 'STOCHRSI(14)', 'Williams %R', 'Ultimate Oscillator', 'Bull/Bear Power(13)', 'MA10', 'MA20', 'MA50']
-    dict2 = {}
+    
 
     
     tables_trading = driver.find_elements(By.CLASS_NAME, "table-hvDpy38G")[:2]
@@ -183,7 +200,7 @@ def scrape_both(driver1, driver):
         
 
         # print("----- End of Table -----")
-    matching_values = {}
+    
     for key1, key2 in simple_keys_dict1.items():
         if key2 in dict2 and dict1[key1] == dict2[key2]:
             matching_values[key2] = dict1[key1]
